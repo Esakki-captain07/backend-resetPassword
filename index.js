df9725aaa7f4491d2bb9ae805a8f0929cb3b5cae
esakki raj <essaki078@gamil.com>
2024-08-12 07:19:27 +0100

    url changes

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ import routes from './src/router/index.js'
 const PORT = process.env.PORT
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://http://localhost:5173/', // Replace with your frontend's local URL if it's different
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+}))
 app.use(express.json())
 app.use(routes)
 
