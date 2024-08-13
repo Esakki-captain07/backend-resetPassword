708536b0048ce7e270848f789292de9025bd6454
esakki raj <essaki078@gamil.com>
2024-08-13 04:07:24 +0100

    update cors

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ import express from 'express'
 import 'dotenv/config.js'
 import cors from 'cors'
 import routes from './src/router/index.js'
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000;
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:5173', // Correct origin for your local frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: 'https://passwordreset-front-end.netlify.app',
+    methods: 'POST',
+    allowedHeaders: 'Content-Type,Authorization'
 }))
 app.use(express.json())
+app.use(express.urlencoded({ extended: true })); 
 app.use(routes)
+
+
 
 app.listen(PORT,()=>console.log(`Server listening at port ${PORT}`))
